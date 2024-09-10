66a0e07fe208e079e60594b67a0e63586308443c
CARLOS <carl.aravena@duocuc.cl>
2024-09-10 00:07:28 -0300

    NavigationExtra y Modal

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { customPasswordValidator } from '../../custom-validators';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit{
   loginForm!: FormGroup;
   data: any;
 
@@ -18,51 +19,29 @@ export class LoginPage implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private alertController: AlertController
-  ) {}
-
-  ngOnInit() {
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, customPasswordValidator()]]
     });
-
-    // Obtiene los parámetros de consulta
-    this.route.queryParams.subscribe(params => {
-      this.data = params;
-      console.log('Datos recibidos:', this.data); // Datos del formulario de registro
-
-      this.checkForRegistrationSuccess();
-    });
   }
 
-  async checkForRegistrationSuccess() {
-    const registrationSuccess = this.data.registrationSuccess;
-    
-    if (registrationSuccess === 'true') {
-      await this.showSuccessAlert();
-      // Limpia los parámetros de consulta
-      this.router.navigate([], {
-        relativeTo: this.route,
-        queryParams: { registrationSuccess: null },
-        queryParamsHandling: 'merge',
-      });
-    }
-  }
-
-  async showSuccessAlert() {
-    const alert = await this.alertController.create({
-      header: 'Registro Exitoso',
-      message: 'El registro se ha realizado correctamente.',
-      buttons: ['OK']
-    });
-
-    await alert.present();
+  ngOnInit() {
   }
 
   async login() {
     if (this.loginForm.valid) {
-      // Aquí puedes implementar la lógica para iniciar sesión
       console.log('Formulario de inicio de sesión válido');
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+
+      const navigationExtras: NavigationExtras = {
+        state: {
+          email: email,
+          password: password
+        }
+      };
+      this.router.navigate(['tienda'], navigationExtras);
     } else {
       // Si el formulario no es válido, muestra una alerta
       await this.showErrorAlert();
--- src/app/pages/tienda/tienda.page.ts
+++ src/app/pages/tienda/tienda.page.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonModal, AnimationController } from '@ionic/angular';
+import { Animation } from '@ionic/core';
 
 @Component({
   selector: 'app-tienda',
   templateUrl: './tienda.page.html',
   styleUrls: ['./tienda.page.scss'],
 })
-export class TiendaPage implements OnInit {
+export class TiendaPage implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('modal', { static: false }) modal!: IonModal;
 
-  ngOnInit() {
+  email!: string;
+  password!: string;
+
+  constructor(private router: Router, private animationCtrl: AnimationController) {
+    // Obtiene los datos del estado de navegación si existen
+    if (this.router.getCurrentNavigation()?.extras.state) {
+      const datosUsuario = this.router.getCurrentNavigation()?.extras.state;
+      this.email = datosUsuario?.['email'];
+      this.password = datosUsuario?.['password'];
+
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+    }
   }
 
-}
+  ngAfterViewInit() {
+    if (this.modal) {
+      const enterAnimation = (baseEl: HTMLElement) => {
+        const root = baseEl.shadowRoot || baseEl;
+
+        const backdropElement = root.querySelector('ion-backdrop');
+        const wrapperElement = root.querySelector('.modal-wrapper');
+
+        const backdropAnimation = backdropElement
+          ? this.animationCtrl
+              .create()
+              .addElement(backdropElement)
+              .fromTo('opacity', '0.01', 'var(--backdrop-opacity)')
+          : null;
+
+        const wrapperAnimation = wrapperElement
+          ? this.animationCtrl
+              .create()
+              .addElement(wrapperElement)
+              .keyframes([
+                { offset: 0, opacity: '0', transform: 'scale(0)' },
+                { offset: 1, opacity: '0.99', transform: 'scale(1)' },
+              ])
+          : null;
+
+        const animations = [backdropAnimation, wrapperAnimation].filter(
+          Boolean
+        ) as Animation[];
+
+        return this.animationCtrl
+          .create()
+          .addElement(baseEl)
+          .easing('ease-out')
+          .duration(500)
+          .addAnimation(animations);
+      };
+
+      const leaveAnimation = (baseEl: HTMLElement) => {
+        return enterAnimation(baseEl).direction('reverse');
+      };
+
+      this.modal.enterAnimation = enterAnimation;
+      this.modal.leaveAnimation = leaveAnimation;
+    } else {
+      console.error('Modal is undefined');
+    }
+  }
+
+  closeModal() {
+    if (this.modal) {
+      this.modal.dismiss();
+    }
+  }
+}  
