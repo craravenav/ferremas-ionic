37fbce6d9a9fc0c169a7815db172630d3ba44ad4
samuel gonzalez <samu.gonzalez@duocuc.cl>
2024-09-11 23:54:13 -0300

    Se agrega page Carrito con logica de navigationExtra a la tienda

--- src/app/pages/carrito/carrito.page.ts
+++ src/app/pages/carrito/carrito.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.page.html',
   styleUrls: ['./carrito.page.scss'],
 })
-export class CarritoPage implements OnInit {
+export class CarritoPage {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  goToHome() {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        // Aquí puedes incluir cualquier dato que quieras pasar a la página de inicio
+        message: 'Bienvenido de vuelta al inicio',
+        cartItems: this.getCartItems() // Ejemplo de datos del carrito
+      }
+    };
+
+    this.router.navigate(['/tienda'], navigationExtras);
   }
 
+  getCartItems() {
+    // Lógica para obtener los artículos del carrito
+    return [
+      { id: 1, name: 'Producto 1', price: 10 },
+      { id: 2, name: 'Producto 2', price: 20 }
+    ];
+  }
 }
